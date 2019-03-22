4c11c375ee073040b09d50d80dde74d53d8a82b3
Sam Mead <samjamead@gmail.com>
2019-03-22 18:31:22 +0800

    Rework

---
+++ README.md
@@ -0,0 +1,14 @@
+# Alan Mead dot co
+
+Alan Mead's website
+
+## Todos
+
+- [ ] Set up Google Analytics
+- [ ] Rebrand on Twitter?
+- [x] LinkedIn not CV
+- [ ] Leave off address? Get a PO box?
+- [ ] Alternate typeface for quotes?
+- [ ] Logo as image not type
+- [ ] Set up G suite and alan@alanmead.co email address
+- [ ] Deploy by deleting Meady777 repo and updating CNAME 
---
+++ _site/assets/scripts.js
@@ -0,0 +1,26 @@
+jQuery( document ).ready(function($){
+  // Smoooooth scrolling
+  $('.smooth-scroll').on("click", function(){
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top - 80
+    }, 500);
+    return false;
+  });
+
+  $(window).scroll(function () {
+    if ($(document).scrollTop() > 300) {
+      $(".site-header").addClass("scrolled");
+      $("#header_logo").attr('src', '/assets/img/logotype.svg');
+    } else {
+      $(".site-header").removeClass("scrolled");
+      $("#header_logo").attr('src', '/assets/img/logotype_white.svg');
+    }
+  });
+
+  var $hamburger = $('.hamburger');
+  $hamburger.on("click", function(){
+    $hamburger.toggleClass("is-active");
+    $('.navbar').toggleClass("menu-open");
+  });
+
+});
