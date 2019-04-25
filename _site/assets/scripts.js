89cb7d6af346fbe1de2849adfcb1c35c4fe47d42
Sam Mead <samjamead@gmail.com>
2019-04-25 12:37:14 +0800

    More fixes (RC1)

--- _site/assets/scripts.js
+++ _site/assets/scripts.js
@@ -2,7 +2,7 @@ jQuery( document ).ready(function($){
   // Smoooooth scrolling
   $('.smooth-scroll').on("click", function(){
     $('html, body').animate({
-      scrollTop: $($(this).attr('href')).offset().top - 80
+      scrollTop: $($(this).attr('href')).offset().top - 60
     }, 500);
     return false;
   });
